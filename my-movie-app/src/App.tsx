298901674e4acc725350ac56b0bf49d3aa0ace99
Michael <michael.gannon13@gmail.com>
2023-10-14 15:47:31 +0100

    added components

--- my-movie-app/src/App.tsx
+++ my-movie-app/src/App.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MovieList from './MovieList';
+import MovieList from './components/movies/list/MovieList';
+import Nav from './components/nav/Nav';
 import { Movie } from './types';
 
 function App() {
@@ -66,29 +66,7 @@ function App() {
   };
   return (
     <div className="App">
-
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top w-100">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Payroc Podcast Inspiration App</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+      <Nav />
       <div className="container">
           <div className="row">
             <div className="col-md-6 offset-md-3">
@@ -137,11 +115,10 @@ function App() {
                 </div>
               )}
           </div>
-        </div>
-
-        <div className="listContainer">
-          <MovieList movieList={movieList} />
-        </div>
+      </div>
+      <div className="listContainer">
+        <MovieList movieList={movieList} />
+      </div>
     </div>
   );
 }
