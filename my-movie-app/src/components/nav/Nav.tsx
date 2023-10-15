ad5f5173f8afb5e1cd047ef62305db7db8f5b367
Michael <michael.gannon13@gmail.com>
2023-10-15 14:19:52 +0100

    added lazy loading to list component

--- my-movie-app/src/components/nav/Nav.tsx
+++ my-movie-app/src/components/nav/Nav.tsx
@@ -8,19 +8,7 @@ function Nav() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Your List</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-          </ul>
-        </div>
+   
       </div>
     </nav>
   );
